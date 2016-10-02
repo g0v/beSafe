ef468dd0c4bd2dbb5e6f1da7507a941c41da6e44
Poga Po <poga.bahamut@gmail.com>
2016-10-02 09:55:11 +0800

    somehow doesn't work when we have multiple feed

--- index.js
+++ index.js
@@ -13,29 +13,42 @@ const KEYCODE_ENTER = 13
 var t = Date.now()
 
 var keys = [
-  '51d5703abc2848dae2fd0511adebf1d28f62bcffa6fa45a49a55bcfbea07a86d',
-  'b7130eb23c5ce42834d1e80b59240f7c8bb9dc8a4b8fdec6f628a2588926641e',
-  '1827968a8ea7271976f4b0c17c18a3cc67fa8b51fb84697ddcafd64ee6e877e2'
+  '13e084e9cfb26d20a7fdfa873e794bd83c9f735c5522e227c5966f159988db74',
+  '11ca8522704537528e5e5b0575564564cfca3af452f353ddfca5973f50cf9283',
+  '529df2efe8f349ffc62a56e80131d98175e78c89fdbe26e3c6a3e02bd72ac896'
 ]
+keys = ['13e084e9cfb26d20a7fdfa873e794bd83c9f735c5522e227c5966f159988db74']
 
 var entries = {}
 var items = []
 var feeds = []
 
 for (var i = 0; i < keys.length; i++) {
-  var feed = new Hyperfeed(keys[i], {storage: level('./feed'), own: false})
-  console.log('opened', keys[i])
-  feed.swarm()
-  feeds[keys[i]] = feed
-  entries[keys[i]] = []
+  (function (key) {
+    var feed = new Hyperfeed(key, {storage: level(`./feed-${key}`), own: false})
+    console.log('opened', key)
+    var sw = feed.swarm()
+    sw.on('connection', function (peer, type) {
+      console.log(`[${feed.key().toString('hex')}]`, 'got', type) // type is 'webrtc-swarm' or 'discovery-swarm'
+      console.log(`[${feed.key().toString('hex')}]`, 'connected to', sw.connections, 'peers')
+      peer.on('close', function () {
+        console.log(`[${feed.key().toString('hex')}]`, 'peer disconnected')
+      })
+    })
+    feed.list((err, entries) => {
+      console.log(err, entries.length)
+    })
+    feeds[key] = feed
+    entries[key] = []
 
-  console.log('listing')
-  var list = feed.list({live: true})
-  list.on('data', entry => {
-    if (moment(entry.ctime) > moment().substract(7, 'days')) {
-      entries[keys[i]].push(entry)
-    }
-  })
+    console.log('listing')
+    var list = feed.list({live: true})
+    list.on('data', entry => {
+      if (moment(entry.ctime) > moment().subtract(3, 'days')) {
+        entries[key].push(entry)
+      }
+    })
+  })(keys[i])
 }
 
 function updateApp () {
