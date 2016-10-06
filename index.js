b02844dff2057da8361ab42c72f18e4f4de1714a
Poga Po <poga.bahamut@gmail.com>
2016-10-06 08:52:03 +0800

    rewrite with 2.0, simplify rendering

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-/* globals jdenticon */
-const Hyperfeed = require('hyperfeed')
+/* globals jdenticon, CalHeatMap */
+const hyperfeed = require('hyperfeed')
+const hyperdrive = require('hyperdrive')
 const level = require('level-browserify')
 const yo = require('yo-yo')
-const co = require('co')
 const crypto = require('crypto')
 const moment = require('moment')
 const async = require('async')
+const _ = require('lodash')
 
 moment.locale('zh-tw')
 
-const KEYCODE_ENTER = 13
+var hf = hyperfeed(hyperdrive(level('feed')))
 
-var t = Date.now()
+var keys = ['93ee801c6d562f29f01dce5c38a60ed61cc0985c97e552dfa54bc75e707effa2']
 
-var keys = ['cee1cbbf213c04c7c3b37a25fc88cdbf0e54d0c170ea05b490b8e1d9b2074923']
-
-var entries = {}
 var items = []
 var feeds = []
+
+var cal = new CalHeatMap()
+cal.init({
+  domain: 'month',
+  colLimit: 31,
+  range: 1,
+  cellSize: 15,
+  displayLegend: false
+})
 
 var tasks = []
 for (var i = 0; i < keys.length; i++) {
@@ -28,10 +35,12 @@ async.series(tasks, (err, connections) => {
   console.log('all connected', connections.length)
 })
 
+var update = _.debounce(updateApp, 500)
+
 function connect (key) {
   return (cb) => {
     console.log('start connect', key)
-    var feed = new Hyperfeed(key, {own: false})
+    var feed = hf.createFeed(key, {own: false})
     console.log('opened', key)
     var sw = feed.swarm()
     console.log('swarming', key)
@@ -45,72 +54,37 @@ function connect (key) {
       cb()
     })
     feeds[key] = feed
-    entries[key] = []
 
     console.log('listing')
     var list = feed.list({live: true})
     list.on('data', entry => {
       if (moment(entry.ctime) > moment().subtract(3, 'days')) {
-        entries[key].push(entry)
-        console.log(entry)
+        feeds[key].load(entry).then(item => {
+          console.log(item)
+          items.push(item)
+
+          update()
+        })
       }
     })
   }
 }
 
 function updateApp () {
-  co(function * () {
-    items = []
-    for (var i = 0; i < Object.keys(entries).length; i++) {
-      var feedKey = Object.keys(entries)[i]
-      for (var j = 0; j < entries[feedKey].length; j++) {
-        var item = yield feeds[feedKey].load(entries[feedKey][j])
-        items.push(item)
-      }
-    }
-    console.log(Date.now() - t)
-    items = items.sort((x, y) => { return y.date - x.date })
+  console.log('updating')
+  items = items.sort((x, y) => { return y.date - x.date })
 
-    render()
-    jdenticon()
-  })
+  render()
+  jdenticon()
 }
 
 function render () {
   console.log('render', items.length)
-  yo.update(document.querySelector('#app'), yo`
-    <div id="app">
-      <div class="ui two column centered grid">
-        <div class="column">
-          <div class="ui feed">${items.map(x => { return renderItem(x) })}</div>
-        </div>
-      </div>
+  yo.update(document.querySelector('#timeline'), yo`
+    <div id="timeline">
+      <div class="ui feed">${items.map(x => { return renderItem(x) })}</div>
     </div>
   `)
-}
-
-function renderInput () {
-  return yo`
-    <div class="ui input">
-      <input
-        type="text"
-        placeholder="Say..."
-        onkeydown=${onInputKeyDown}>
-    </div>
-  `
-}
-
-function onInputKeyDown (e) {
-  if (e.keyCode === KEYCODE_ENTER) {
-    e.preventDefault()
-    var value = e.target.value
-
-    co(function * () {
-      yield feed.push({ title: value, author: { name: feed.key().toString('hex') } })
-      e.target.value = ''
-      updateApp()
-    })
-  }
 }
 
 function renderItem (x) {
@@ -136,5 +110,3 @@ function renderItem (x) {
     </div>
   `
 }
-
-setInterval(updateApp, 10000)
