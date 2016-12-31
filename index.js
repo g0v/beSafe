068297a548cdbfdcfb37af40954ba42005e75109
Poga Po <poga.bahamut@gmail.com>
2016-12-31 23:24:52 +0800

    rewrite with websocket

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* globals jdenticon, CalHeatMap, Modernizr */
+/* globals jdenticon, CalHeatMap, WebSocket */
 const hyperfeed = require('hyperfeed')
 const hyperdrive = require('hyperdrive')
 const level = require('level-browserify')
@@ -8,109 +8,45 @@ const moment = require('moment')
 const async = require('async')
 const _ = require('lodash')
 
-if (!Modernizr.peerconnection) {
-  yo.update(document.querySelector('#timeline'), yo`
-    <div id="timeline" class="ui basic center aligned segment">
-      <h3>此瀏覽器不支援 WebRTC</h3>
-      <div>
-        We're working on it. Please try again with Google Chrome or Firefox.
-      </div>
-    </div>
-  `)
-}
-
 moment.locale('zh-tw')
 
-var db = level('feed')
-var hf = hyperfeed(hyperdrive(db))
-
-function updateStats (connCount) {
-  yo.update(document.querySelector('#stats'), yo`
-    <div id="stats" class="desc">
-      ${connCount} 人連線中
-    </div>
-  `)
-}
-
-var keys = ['b84132a5a94ef69387e9419aa994378710268993714e7532cb43c3922467c873']
-
 var items = []
-var feeds = []
 window.items = items
 
 var heatmap = {}
 
 var cal = new CalHeatMap()
 cal.init({
-  domain: 'month',
-  colLimit: 31,
+  domain: 'week',
+  colLimit: 7,
   range: 1,
   cellSize: 15,
   displayLegend: false,
   tooltip: true
 })
 
-var tasks = []
-for (var i = 0; i < keys.length; i++) {
-  tasks.push(connect(keys[i]))
-}
-
-async.series(tasks, (err, connections) => {
-  console.log('all connected', connections.length)
-})
-
 var update = _.debounce(updateApp, 200)
 
 var loaded = 0
 
-function connect (key) {
-  return (cb) => {
-    var firstConnection = true
-    console.log('start connect', key)
-    var feed = hf.createFeed(key, {own: false})
-    console.log('opened', key)
-    var sw = feed.swarm()
-    console.log('swarming', key)
-    sw.on('error', e => console.error(e))
-    sw.on('connection', function (peer, type) {
-      updateStats(sw.connections)
-      console.log(`[${feed.key().toString('hex')}]`, 'got', type) // type is 'webrtc-swarm' or 'discovery-swarm'
-      console.log(`[${feed.key().toString('hex')}]`, 'connected to', sw.connections, 'peers')
-      peer.on('close', function () {
-        updateStats(sw.connections)
-        console.log(`[${feed.key().toString('hex')}]`, 'peer disconnected')
-      })
-      if (firstConnection) {
-        firstConnection = false
-        cb()
-      }
-    })
-    feeds[key] = feed
-    window.feed = feed
+connect('ws://devpoga.org:9090')
 
-    var list = feed.list({live: true})
-    var metadataLimit = moment().subtract(1, 'month')
-    var loadLimit = moment().subtract(1, 'day')
-    list.on('data', entry => {
-      var loadMsg = document.querySelector('#loading_msg')
-      if (loadMsg) loadMsg.innerHTML = `讀取中 (${loaded})...`
-      update()
+function connect (url) {
+  var socket = new WebSocket(url)
+  socket.onmessage = function (e) {
+    var entry = JSON.parse(e.data)
 
-      var ctime = moment(entry.ctime)
-      if (ctime > metadataLimit) {
-        loaded += 1
-        var time = entry.ctime / 1000
-        if (!heatmap[time]) heatmap[time] = 0
-        heatmap[time] += 1
-      }
-      if (ctime > loadLimit) {
-        feeds[key].load(entry).then(item => {
-          items.push(item)
+    var loadMsg = document.querySelector('#loading_msg')
+    if (loadMsg) loadMsg.innerHTML = `讀取中 (${loaded})...`
+    update()
 
-          update()
-        })
-      }
-    })
+    loaded += 1
+    var time = entry.ctime / 1000
+    if (!heatmap[time]) heatmap[time] = 0
+    heatmap[time] += 1
+    items.push(entry.item)
+
+    update()
   }
 }
 
